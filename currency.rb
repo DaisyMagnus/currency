8e842b84c85baa675f3a8efe7542f310a24479fa
Daisy Magnus-Aryitey <daisymagnusaryitey@gmail.com>
2015-05-07 13:45:23 -0400

    Add two methods for amount and currency code

--- currency.rb
+++ currency.rb
@@ -3,3 +3,18 @@ class Currency
     @amount = amount
     @currency_code = currency_code
   end
+
+
+def amount
+  @amount
+end
+
+def currency_code
+  @currency_code
+end
+
+
+currency = Currency.new(2, "USD")
+puts currency.amount
+
+end
