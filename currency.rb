7a3b0d1e789a5c52d8e2dc1c60a1721d6ff39f73
Daisy Magnus-Aryitey <daisymagnusaryitey@gmail.com>
2015-05-10 02:52:41 -0400

    Add better multplication method

--- currency.rb
+++ currency.rb
@@ -35,55 +35,23 @@ class Currency
    		p "DifferentCurrencyCodeError"
 
     end
+
+  def -(other)
+   	if @amount.to_s[0] == other.to_s[0] && @amount > other
+   		  p @amount.to_s[0] + (@amount.slice(@amount[1..-1]).to_i - other.slice(other[1..-1]).to_i).to_s
+   	end
+   		p "DifferentCurrencyCodeError"
+
+  def *(other)
+    if @amount.to_s[0] == other.to_s[0]
+      @amount.to_s[0] + (@amount.slice(@amount[1..-1]).to_i * other.slice(other[1..-1]).to_i).to_s
+    end
+  end
+
+def symbol_conversion
+    @codes = {"¢"=> "GHC", "$"=> "USD", "¥" => "JPY"}
+    @codes[amount[0]]
+    p amount[0].replace(@codes[amount[0]]) + amount[1..-1]
 end
 
-# class Currency
-#   attr_reader :amount, :currency_code
-#   def initialize (amount = 0.0, currency_code = " ")
-#     @amount = amount.to_i
-#     @currency_code = currency_code.to_s
-#   end
-#
-# def symbol_conversion
-#     @codes = {"¢"=> "GHC", "$"=> "USD", "¥" => "JPY"}
-#     @codes[amount[0]]
-#     p amount[0].replace(@codes[amount[0]]) + amount[1..-1]
-# end
-#
-# def ==(other)
-#   if self.amount == other.amount  && self.currency_code == other.currency_code
-#     return true
-#   else
-#     false
-#   end
-# end
-#
-#
-# def +(other)
-#    if self.currency_code == other.currency_code
-#      return Currency.new(currency_code, self.amount + other.amount)
-#    else
-#      raise "DifferentCurrencyCodeError"
-#    end
-# end
-#
-#
-# def -(other)
-#    if self.currency_code == other.currency_code
-#      return Currency.new(currency_code, self.amount.to_f - other.amount.to_f)
-#    else
-#      raise "DifferentCurrencyCodeError"
-#    end
-# end
-#
-# def *(other)
-#    if self.currency_code == other.currency_code
-#      return Currency.new(currency_code, self.amount.to_f * other.amount.to_f)
-#    else
-#      puts "DifferentCurrencyCodeError"
-#    end
-# end
-#
-#
-#
-# end
+end
